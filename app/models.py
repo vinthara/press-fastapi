0db361f95871a05b4ec23b761fe3c36d6196cc7b
Aravinth BALAKRISHNAN <aravinthbalakrishnan@gmail.com>
2023-04-22 23:31:27 +0200

    Add EMPLOYEE PRESS EMPLOYEE_ROLE tables

--- app/models.py
+++ app/models.py
@@ -5,8 +5,8 @@ from sqlalchemy.orm import relationship
 from .database import Base
 
 
-class User(Base):
-    __tablename__ = "USER"
+class Employee(Base):
+    __tablename__ = "EMPLOYEE"
 
     id = Column(Integer, primary_key=True, nullable=False)
     first_name = Column(String, nullable=False)
@@ -14,4 +14,25 @@ class User(Base):
     nickname = Column(String, nullable=True)
     email = Column(String, unique=True, nullable=False)
     password = Column(String, nullable=False)
-    is_employed = Column(Boolean, nullable=False, default=True)
+    is_super_admin = Column(Boolean, nullable=False, default=False)
+
+
+class Press(Base):
+    __tablename__ = "PRESS"
+
+    id = Column(String, primary_key=True, nullable=True)
+    name = Column(String, nullable=False)
+    address = Column(String, nullable=False)
+    city = Column(String, nullable=False)
+    zip_code = Column(Integer, nullable=False)
+
+
+class Employee_role(Base):
+    __tablename__ = "EMPLOYEE_ROLE"
+
+    employee_id = Column(
+        Integer, ForeignKey("EMPLOYEE.id"), primary_key=True, nullable=False
+    )
+    press_id = Column(String, ForeignKey("PRESS.id"), primary_key=True, nullable=False)
+    is_employee = Column(Boolean, nullable=False, default=False)
+    is_admin = Column(Boolean, nullable=False, default=False)
