1521efa8bc4370de24ba281a2555bdce7a6c5796
Aravinth BALAKRISHNAN <aravinthbalakrishnan@gmail.com>
2023-04-23 23:51:35 +0200

    Update user with employee

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -8,24 +8,24 @@ router = APIRouter(tags=["Authentication"])
 
 @router.post("/login")
 def login(
-    user_credentials: OAuth2PasswordRequestForm = Depends(),
+    employee_credentials: OAuth2PasswordRequestForm = Depends(),
     db: Session = Depends(database.get_db),
 ):
-    user = (
+    employee = (
         db.query(models.Employee)
-        .filter(models.Employee.email == user_credentials.username)
+        .filter(models.Employee.email == employee_credentials.username)
         .first()
     )
-    if not user:
+    if not employee:
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN, detail="Invalid Credentials"
         )
 
-    if not utils.verify(user_credentials.password, user.password):
+    if not utils.verify(employee_credentials.password, employee.password):
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN, detail="Invalid Credentials"
         )
 
-    access_token = oauth2.create_access_token(data={"user_id": user.id})
+    access_token = oauth2.create_access_token(data={"employee_id": employee.id})
 
     return {"access_token": access_token, "token_type": "bearer"}
